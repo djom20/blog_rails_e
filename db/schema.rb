a73e644f6ffcbfa3eef40102b86a9dcef811958b
urielhdz <projectdxv@gmail.com>
2012-09-24 06:58:10 +0400

    assets compiled

--- db/schema.rb
+++ db/schema.rb
@@ -52,10 +52,12 @@ ActiveRecord::Schema.define(:version => 20120918184940) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
+    t.integer  "article_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "article_id"
   end
+
+  add_index "comments", ["article_id"], :name => "index_comments_on_article_id"
 
   create_table "slugs", :force => true do |t|
     t.string   "name"
